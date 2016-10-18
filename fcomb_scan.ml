20c3a99efa7c3869f4ebe2b44987ea5121a0be73
Anton Yabchinskiy <arn@bestmx.net>
2016-10-18 12:30:44 +0300

    Scan: add scan_literal internal fun, implement pair and triplet

--- fcomb_scan.ml
+++ fcomb_scan.ml
@@ -9,6 +9,8 @@ let scan_aux ch fmt =
   with (Scanf.Scan_failure _) -> raise Error
      | IO.Input_closed -> raise Error
 
+let scan_literal ch str = scan_aux ch (Scanf.format_from_string str "%s")
+
 let string ch =
   match scan_aux ch " %s" with
   | "" -> raise Error
@@ -25,10 +27,31 @@ let bit ch = int ch <> 0
 let big_int ch = Big_int.big_int_of_string (string ch)
 let num ch = Num.num_of_string (string ch)
 
-val array : ?first:string -> ?sep:string -> ?last:string -> ?n:int -> 'a t -> 'a array t
-val hpair : ?first:string -> ?sep:string -> ?last:string -> 'a t -> ('a * 'a) t
-val htriplet : ?first:string -> ?sep:string -> ?last:string -> 'a t -> ('a * 'a * 'a) t
-val line : 'a t -> 'a t
-val list : ?first:string -> ?sep:string -> ?last:string -> ?n:int -> 'a t -> 'a list t
-val pair : ?first:string -> ?sep:string -> ?last:string -> 'a t -> 'b t -> ('a * 'b) t
-val triplet : ?first:string -> ?sep:string -> ?last:string -> 'a t -> 'b t -> 'c t -> ('a * 'b * 'c) t
+(* let enum ?(first=" ") ?(sep=" ") ?(last=" ") ?n f ch = *)
+
+
+(* val array : ?first:string -> ?sep:string -> ?last:string -> ?n:int -> 'a t -> 'a array t *)
+(* val hpair : ?first:string -> ?sep:string -> ?last:string -> 'a t -> ('a * 'a) t *)
+(* val htriplet : ?first:string -> ?sep:string -> ?last:string -> 'a t -> ('a * 'a * 'a) t *)
+
+let line f ch = f (IO.input_string (IO.read_line ch))
+
+(* val list : ?first:string -> ?sep:string -> ?last:string -> ?n:int -> 'a t -> 'a list t *)
+
+let pair ?(first=" ") ?(sep=" ") ?(last=" ") f g ch =
+  ignore (scan_literal ch first);
+  let a = f ch in
+  ignore (scan_literal ch sep);
+  let b = g ch in
+  ignore (scan_literal ch last);
+  (a, b)
+
+let triplet ?(first=" ") ?(sep=" ") ?(last=" ") f g h ch =
+  ignore (scan_literal ch first);
+  let a = f ch in
+  ignore (scan_literal ch sep);
+  let b = g ch in
+  ignore (scan_literal ch sep);
+  let c = h ch in
+  ignore (scan_literal ch last);
+  (a, b, c)
